8c7745166f350c9b733eba7c03372fad70b08443
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2023-11-22 12:38:21 -0300

    fix: fix question10test spacing

--- question10/Question10Test.java
+++ question10/Question10Test.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class Question10Test {
-    
+
     @ParameterizedTest
     @MethodSource("provideDataForEvaluateLengthString")
     public void testEvaluateLengthString(String input, int expected) {
@@ -20,9 +20,8 @@ public class Question10Test {
 
     public static Stream<Arguments> provideDataForEvaluateLengthString() {
         return Stream.of(
-            Arguments.of("hello", 5),
-            Arguments.of("HelloWorld", 10),
-            Arguments.of("Mateus", 6)
-        );
+                Arguments.of("hello", 5),
+                Arguments.of("HelloWorld", 10),
+                Arguments.of("Mateus", 6));
     }
 }
